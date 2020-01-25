f7c219c5b8d2ca0f674dbc58c0d1fce72b92ed6a
Kishor Pahalwani <kishore.pahalwani@impelsys.com>
2020-01-25 09:14:33 +0530

    Initial commit

---
+++ Spectrum_Details/Spectrum_Details/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Spectrum_Details
+//
+//  Created by Kishor Pahalwani on 25/01/20.
+//  Copyright © 2020 Kishor Pahalwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
